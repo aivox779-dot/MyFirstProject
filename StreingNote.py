0ef59a3a014d6c1672f1d8d2dbd5ee3d315ab7b4
M-Hani <Aivox779@gmail.com>
2025-08-13 01:09:50 +0300

    Initial commit

---
+++ StreingNote.py
@@ -0,0 +1,13 @@
+Phersis1= "Mohmmed  is so smart"
+Phersis2 = " He is age is 18 "
+Phersis3 = " HE IS THE SMAERTEST "
+
+print (Phersis1 + "\t" + "," + "\"" + Phersis2 + "\""  + "\n" + Phersis3)
+print (Phersis1.lower())
+print (Phersis2.upper())
+print (Phersis3.islower()) 
+print (Phersis3.isupper())
+print (len(Phersis1))
+print (Phersis2[8])
+print (Phersis3.index("H")) 
+print (Phersis2.replace("is" , "Are"))
---
+++ trojekt.py
@@ -0,0 +1,10 @@
+from math import *
+from decimal import Decimal 
+num1 = 7
+num2 = 3
+print(" Welcome Mohmmed ") 
+print ("7+3 = " ,num1 + num2)
+print ("7+3 = " ,num1 - num2)
+print ("7+3 = " ,num1 * num2)
+print ("7+3 = " ,num1 / num2)
+print ("7+3 = " ,num1 % num2)
